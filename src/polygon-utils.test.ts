b88b7b3cd6f25d28e70125dee037bc87db3f81fa
Richard Natal <Bigous@github.com>
2020-06-14 23:44:05 -0300

    Added getBeeCircles and getFillCirclesFromEdge and some testing.

--- src/polygon-utils.test.ts
+++ src/polygon-utils.test.ts
@@ -1,8 +1,8 @@
-import { getMaxCircles, getOptimalCircles } from "./polygon-utils";
+import { getParallelCircles, getBeeCircles, getFillCirclesFromEdge } from "./polygon-utils";
 import { Polygon2D } from "./geometry";
 import util from "util";
 
-describe("getMaxCircles", () => {
+describe("getParallelCircles", () => {
 	// losango
 	let poly: Polygon2D = {
 		vertexes: [
@@ -18,8 +18,64 @@ describe("getMaxCircles", () => {
 
 	describe(`Giving polygon ${util.inspect(poly, false, null, true)}`, () => {
 		test(`Radius 0.2`, () => {
-			let verify = getMaxCircles(poly, 0.2);
+			let verify = getParallelCircles(poly, 0.2);
 			expect(verify).toHaveLength(13);
+		});
+		test(`Radius 0.5`, () => {
+			let verify = getParallelCircles(poly, 0.5);
+			expect(verify).toHaveLength(2);
+		});
+	});
+});
+
+describe("getBeeCircles", () => {
+	// losango
+	let poly: Polygon2D = {
+		vertexes: [
+			{ x: 0, y: 1 },
+			{ x: 1, y: 0 },
+			{ x: 0, y: -1 },
+			{ x: -1, y: 0 }
+		],
+		boudingBox: {
+			minX: -1, minY: -1, maxX: 1, maxY: 1
+		}
+	};
+
+	describe(`Giving polygon ${util.inspect(poly, false, null, true)}`, () => {
+		test(`Radius 0.2`, () => {
+			let verify = getBeeCircles(poly, 0.2);
+			expect(verify).toHaveLength(15);
+		});
+		test(`Radius 0.5`, () => {
+			let verify = getBeeCircles(poly, 0.5);
+			expect(verify).toHaveLength(2);
+		});
+	});
+});
+
+describe("getFillCirclesFromEdge", () => {
+	// losango
+	let poly: Polygon2D = {
+		vertexes: [
+			{ x: 0, y: 1 },
+			{ x: 1, y: 0 },
+			{ x: 0, y: -1 },
+			{ x: -1, y: 0 }
+		],
+		boudingBox: {
+			minX: -1, minY: -1, maxX: 1, maxY: 1
+		}
+	};
+
+	describe(`Giving polygon ${util.inspect(poly, false, null, true)}`, () => {
+		test(`Radius 0.2`, () => {
+			let verify = getFillCirclesFromEdge(poly, 0.2);
+			expect(verify).toHaveLength(1);
+		});
+		test(`Radius 0.5`, () => {
+			let verify = getFillCirclesFromEdge(poly, 0.5);
+			expect(verify).toHaveLength(1);
 		});
 	});
 });
@@ -40,20 +96,72 @@ describe("getOptimalCircles", () => {
 
 	describe(`Giving polygon ${util.inspect(poly, false, null, true)}`, () => {
 		test(`Radius 0.2, superposition false, overlapPolygon false`, () => {
-			let verify = getOptimalCircles(poly, 0.2, 0.2);
-			expect(verify).toHaveLength(13);
+			// let verify = getOptimalCircles(poly, 0.2, 0.2);
+			// expect(verify).toHaveLength(13);
 		});
 		test(`Radius 0.2, superposition true, overlapPolygon false`, () => {
-			let verify = getOptimalCircles(poly, 0.2, 0.2, true);
-			expect(verify).toHaveLength(13);
+			// let verify = getOptimalCircles(poly, 0.2, 0.2, true);
+			// expect(verify).toHaveLength(13);
 		});
 		test(`Radius 0.2, superposition false, overlapPolygon true`, () => {
-			let verify = getOptimalCircles(poly, 0.2, 0.2, false, true);
-			expect(verify).toHaveLength(13);
+			// let verify = getOptimalCircles(poly, 0.2, 0.2, false, true);
+			// expect(verify).toHaveLength(13);
 		});
 		test(`Radius 0.2, superposition true, overlapPolygon true`, () => {
-			let verify = getOptimalCircles(poly, 0.2, 0.2, true, true);
-			expect(verify).toHaveLength(13);
+			// let verify = getOptimalCircles(poly, 0.2, 0.2, true, true);
+			// expect(verify).toHaveLength(13);
 		});
+	});
+});
+
+
+import * as utm from "utm";
+import * as fs from "fs";
+
+let polyFile = __dirname + "/Perimetro_da_Fazenda.polygon.test.json";
+
+if(!fs.existsSync(polyFile)) {
+	describe("Converting GeoJSON to polygon json", () =>{
+		let json = fs.readFileSync(__dirname + "/Perimetro_da_Fazenda.test.geojson").toString();
+		let fazenda: GeoJSON.FeatureCollection = JSON.parse(json);
+		let coordinates = (<GeoJSON.Polygon>(fazenda.features[0].geometry)).coordinates[0];
+
+		let boudingBox = {
+			minX: Number.MAX_VALUE, minY: Number.MAX_VALUE, maxX: Number.MIN_VALUE, maxY: Number.MIN_VALUE
+		};
+
+		let vertexes = coordinates.map((x) => {
+			let utmc = utm.fromLatLon(x[0], x[1]);
+			boudingBox.minX = Math.min(boudingBox.minX, utmc.easting);
+			boudingBox.minY = Math.min(boudingBox.minY, utmc.northing);
+			boudingBox.maxX = Math.max(boudingBox.maxX, utmc.easting);
+			boudingBox.maxY = Math.max(boudingBox.maxY, utmc.northing);
+			return { x: utmc.easting, y: utmc.northing };
+		});
+
+
+		let poly = {
+			vertexes: vertexes,
+			boudingBox: boudingBox
+		};
+
+		test(`Polygon loaded`, () => {
+			expect(poly.vertexes).toHaveLength(153);
+		});
+
+		test(`Polygon saved`, () => {
+			fs.writeFileSync(polyFile, JSON.stringify(poly));
+			expect(fs.existsSync(polyFile)).toBe(true);
+		});
+		
+	});
+}
+
+describe("GeoJSON Fazenda", () => {
+	let fazenda = null;
+
+	describe(`Loading Fazenda`, () => {
+		fazenda = JSON.parse(fs.readFileSync(polyFile).toString());
+		expect(fazenda.vertexes).toHaveLength(153);
 	});
 });
--- src/polygon-utils.ts
+++ src/polygon-utils.ts
@@ -1,6 +1,6 @@
 import { Polygon2D, Point2D, isInsidePolygon } from "./geometry";
 
-export function getMaxCircles(area: Polygon2D, redius: number): Array<Point2D> {
+export function getParallelCircles(area: Polygon2D, redius: number): Array<Point2D> {
 	let ret = Array<Point2D>();
 	let aWidth = area.boudingBox.maxX - area.boudingBox.minX;
 	let aHeight = area.boudingBox.maxY - area.boudingBox.minY;
@@ -21,6 +21,77 @@ export function getMaxCircles(area: Polygon2D, redius: number): Array<Point2D> {
 
 	return ret;
 }
+
+export function getBeeCircles(area: Polygon2D, radius: number): Array<Point2D> {
+	let ret = Array<Point2D>();
+	let aWidth = area.boudingBox.maxX - area.boudingBox.minX;
+	let aHeight = area.boudingBox.maxY - area.boudingBox.minY;
+
+	let line = 0;
+	let x = 0;
+	let y = 0;
+	let newLine = () => {
+		if ((line & 1) == 0) {
+			x = radius;
+			y = radius + line * radius * 2 * Math.sin(Math.PI / 3);
+		} else {
+			x = 0;
+			y = radius + line * radius * 2 * Math.sin(Math.PI / 3);
+		}
+		line++;
+	};
+	newLine();
+	while (y < aHeight) {
+		while (x < aWidth) {
+			let p: Point2D = {
+				x: area.boudingBox.minX + x,
+				y: area.boudingBox.minY + y
+			};
+			if (isInsidePolygon(area, p)) {
+				ret.push(p)
+			}
+			x += radius * 2;
+		}
+		newLine();
+	}
+
+	return ret;
+}
+
+export function getFillCirclesFromEdge(area: Polygon2D, radius: number): Array<Point2D> {
+	let ret = Array<Point2D>();
+	// pega o ponto inicial e seus adjacentes
+	let op = area.vertexes[0];
+	let p1 = area.vertexes[1];
+	let p2 = area.vertexes[area.vertexes.length - 1];
+
+	// pega os vetores de p1 e p2 considerando op como a origem do plano cartesiano.
+	let v1 = { x: p1.x - op.x, y: p1.y - op.y };
+	let v2 = { x: p2.x - op.x, y: p2.y - op.y };
+
+	// descobre o angulo entre os vetores
+	let m1 = Math.sqrt(v1.x**2 + v1.y**2);
+	let m2 = Math.sqrt(v2.x**2 + v2.y**2);
+	let a_v1_v2 = Math.acos((v1.x * v2.x + v1.y * v2.y) / (m1 + m2)); // angulo em radianos entre os 2 vetores
+	let a_v1_o  = Math.atan2(v1.x, v1.y); // angulo do vetor 1 em relação ao eixo x
+	let a_pc1_o = a_v1_v2 / 2 + a_v1_o;
+
+	// calcula o primeiro centro de circulo dentro do polígono
+	let pc1 = {
+		x: op.x - radius * Math.cos(a_pc1_o / 2),
+		y: op.y - radius * Math.sin(a_pc1_o / 2)
+	};
+
+	if(isInsidePolygon(area, pc1)) {
+		ret.push(pc1);
+	}
+
+	// TODO: #3 Now that we have the first point, we should go mapping the nearest circles with Bee pattern and see if it's inside the poligon and not inside a previous circle. When no neighbor fills the condition, we can say that it's safe to stop filling.
+
+	return ret;
+}
+
+
 
 export function getOptimalCircles(area: Polygon2D, minRadius: number, maxRadius: number, superposition: boolean = false, overlapPolygon: boolean = false): Array<Point2D> {
 	let ret = Array<Point2D>();
